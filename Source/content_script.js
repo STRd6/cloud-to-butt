8fc831e64ee3c52763ac873ed1351d1c31fbf122
Daniel X Moore <yahivin@gmail.com>
2016-07-10 10:36:58 -0700

    Screenies and package

--- Source/content_script.js
+++ Source/content_script.js
@@ -2,39 +2,39 @@ walk(document);
 
 function walk(node)
 {
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
 
-	var child, next;
+  var child, next;
 
-	switch ( node.nodeType )
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child )
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+  switch ( node.nodeType )
+  {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while ( child )
+      {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
 }
 
 function handleText(textNode)
 {
-	var v = textNode.nodeValue;
+  var v = textNode.nodeValue;
 
-	v = v.replace(/\bIoT\b/ig, "scrotum");
-	v = v.replace(/\bInternet of Things\b/g, "Pair of Testicles");
-	v = v.replace(/\bInternet of things\b/g, "Pair of testicles");
-	v = v.replace(/\binternet of things\b/ig, "pair of testicles");
+  v = v.replace(/\bIoT\b/ig, "scrotum");
+  v = v.replace(/\bInternet of Things\b/g, "Pair of Testicles");
+  v = v.replace(/\bInternet of things\b/g, "Pair of testicles");
+  v = v.replace(/\binternet of things\b/ig, "pair of testicles");
 
-	textNode.nodeValue = v;
+  textNode.nodeValue = v;
 }
